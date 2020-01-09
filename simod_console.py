8b235a490471ac58a4924bee54473bff0cb9e704
Manuel Camargo <manuel.camargo@ut.ee>
2020-01-09 22:03:33 +0100

    Update simod_console.py

--- simod_console.py
+++ simod_console.py
@@ -14,7 +14,11 @@ from support_modules import support as sup
 # =============================================================================
 # Main function
 # =============================================================================
+
+
 def main(argv):
+    # TODO: https://stats.stackexchange.com/questions/798/calculating-optimal-number-of-bins-in-a-histogram
+    # Revisar como determinar de forma automatica el numero de bins, esto puede ayudar a ajustar mas rapido y mejor los datos
     settings = dict()
     args = dict()
     # Similarity btw the resources profile execution (Song e.t. all)
@@ -22,19 +26,20 @@ def main(argv):
     settings = define_general_settings(settings)
     # Exec mode 'single', 'optimizer'
     settings['exec_mode'] = 'single'
-    # Parameters setting manual fixed or catched by console for batch operations
+    # Parameters setting manual or catched by console for batch operations
     if not argv:
-    #   Event-log filename    
+        # Event-log filename
         settings['file'] = 'PurchasingExample.xes.gz'
         settings['repetitions'] = 1
         settings['simulation'] = True
         if settings['exec_mode'] == 'single':
-            # Splitminer settings [0..1]   
+            # Splitminer settings [0..1]
             settings['epsilon'] = 0.5
             settings['eta'] = 0.3
             # 'removal', 'replacement', 'repairment'
             settings['alg_manag'] = 'repairment'
-            # Processing time definition method: 'manual', 'automatic', 'semi-automatic', 'apx'
+            # Processing time definition method:
+            # 'manual', 'automatic', 'semi-automatic', 'apx'
             settings['pdef_method'] = 'automatic'
             # Single Execution
             # sim.single_exec(settings)
@@ -46,17 +51,20 @@ def main(argv):
             settings['temp_file'] = sup.file_id(prefix='OP_')
             settings['pdef_method'] = 'automatic'
             # Execute optimizer
-            if not os.path.exists(os.path.join('outputs', settings['temp_file'])):
-                open(os.path.join('outputs', settings['temp_file']), 'w').close()
+            if not os.path.exists(os.path.join('outputs',
+                                               settings['temp_file'])):
+                open(os.path.join('outputs',
+                                  settings['temp_file']), 'w').close()
                 sim.hyper_execution(settings, args)
     else:
-#       Catch parameters by console
+        # Catch parameters by console
         try:
             opts, _ = getopt.getopt(argv, "hf:e:n:m:r:",
-                  ['eventlog=', "epsilon=", "eta=", "alg_manag=", "repetitions="])
+                                    ['eventlog=', "epsilon=", "eta=",
+                                     "alg_manag=", "repetitions="])
             for opt, arg in opts:
                 key = catch_parameter(opt)
-                if key in ['epsilon','eta']:
+                if key in ['epsilon', 'eta']:
                     settings[key] = float(arg)
                 elif key == 'repetitions':
                     settings[key] = int(arg)
@@ -67,41 +75,52 @@ def main(argv):
             sys.exit(2)
         settings['simulation'] = True
         sim.single_exec(settings)
-    
 # =============================================================================
 # Support
 # =============================================================================
 
+
 def catch_parameter(opt):
     """Change the captured parameters names"""
-    switch = {'-h':'help', '-f':'file', '-e':'epsilon',
-              '-n':'eta', '-m':'alg_manag', '-r':'repetitions'}
+    switch = {'-h': 'help', '-f': 'file', '-e': 'epsilon',
+              '-n': 'eta', '-m': 'alg_manag', '-r': 'repetitions'}
     try:
         return switch[opt]
-    except:
+    except Exception as e:
+        print(e.message)
         raise Exception('Invalid option ' + opt)
+
 
 def define_general_settings(settings):
     """ Sets the app general settings"""
-    column_names = {'Case ID':'caseid', 'Activity':'task',
-                    'lifecycle:transition':'event_type', 'Resource':'user'}
+    column_names = {'Case ID': 'caseid', 'Activity': 'task',
+                    'lifecycle:transition': 'event_type', 'Resource': 'user'}
     # Event-log reading options
     settings['read_options'] = {'timeformat': '%Y-%m-%dT%H:%M:%S.%f',
-                                'column_names':column_names,
+                                'column_names': column_names,
                                 'one_timestamp': False,
-                                'reorder':False,
-                                'filter_d_attrib':True,
-                                'ns_include':True}
+                                'reorder': False,
+                                'filter_d_attrib': True,
+                                'ns_include': True}
     # Folders structure
     settings['input'] = 'inputs'
     settings['output'] = os.path.join('outputs', sup.folder_id())
-    # External tools routes 
-    settings['miner_path'] = os.path.join('external_tools', 'splitminer', 'splitminer.jar')
-    settings['bimp_path'] = os.path.join('external_tools', 'bimp', 'qbp-simulator-engine.jar')
-    settings['align_path'] =  os.path.join('external_tools', 'proconformance', 'ProConformance2.jar')
-    settings['aligninfo'] = os.path.join(settings['output'], 'CaseTypeAlignmentResults.csv')
-    settings['aligntype'] = os.path.join(settings['output'], 'AlignmentStatistics.csv')
+    # External tools routes
+    settings['miner_path'] = os.path.join('external_tools',
+                                          'splitminer',
+                                          'splitminer.jar')
+    settings['bimp_path'] = os.path.join('external_tools',
+                                         'bimp',
+                                         'qbp-simulator-engine.jar')
+    settings['align_path'] = os.path.join('external_tools',
+                                          'proconformance',
+                                          'ProConformance2.jar')
+    settings['aligninfo'] = os.path.join(settings['output'],
+                                         'CaseTypeAlignmentResults.csv')
+    settings['aligntype'] = os.path.join(settings['output'],
+                                         'AlignmentStatistics.csv')
     return settings
-    
+
+
 if __name__ == "__main__":
     main(sys.argv[1:])
