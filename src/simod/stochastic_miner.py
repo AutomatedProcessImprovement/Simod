663c34be563aff50d633198443a7f4026eb9a4e4
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-07-26 17:54:01 +0300

    Fixing an issue with the incomplete simulation info in BPMN output

--- src/simod/stochastic_miner.py
+++ src/simod/stochastic_miner.py
@@ -1,14 +1,15 @@
 import os
 from dataclasses import dataclass, field
 
-from simod.structure_optimizer import StructureOptimizer
-
 from .cli_formatter import print_step
-from .configuration import Configuration
+from .configuration import Configuration, PDFMethod
 from .decorators import safe_exec_with_values_and_status, timeit
-from .parameter_extraction import Operator
+from .parameter_extraction import Operator, GatewayProbabilitiesMiner, TasksProcessor, InterArrivalMiner, \
+    ParameterExtractionOutput, ParameterExtractionInput
 from .parameter_extraction import Pipeline
 from .stochastic_miner_datatypes import BPMNGraph
+from .structure_optimizer import StructureOptimizer, LogReplayerForStructureOptimizer, \
+    ResourceMinerForStructureOptimizer
 from .writers import xml_writer
 
 
@@ -304,19 +305,37 @@ class StructureOptimizerForStochasticProcessMiner(StructureOptimizer):
     @timeit(rec_name='EXTRACTING_PARAMS')
     @safe_exec_with_values_and_status
     def _extract_parameters(self, settings: dict, structure, parameters, **kwargs) -> None:
-        # TODO: is it computationally safe to call .get_raw_traces() instead of .get_traces()?
-        input = ParameterExtractionInputForStochasticMiner(
-            log_traces=self.log_train.get_raw_traces(), bpmn=self.bpmn_graph)
-        output = ParameterExtractionOutputForStochasticMiner()
-        extraction_pipeline = Pipeline(input=input, output=output)
-        extraction_pipeline.set_pipeline([
-            GatewayProbabilitiesMinerForStochasticMiner
-        ])
-        extraction_pipeline.execute()
+        if isinstance(settings, dict):
+            settings = Configuration(**settings)
 
-        print_step('Rewriting the model')
-        parameters = {'sequences': output.sequences}
-        bpmn_path = os.path.join(settings['output'], settings['project_name'] + '.bpmn')
+        bpmn, process_graph = structure
+        num_inst = len(self.log_valdn.caseid.unique())
+        start_time = self.log_valdn.start_timestamp.min().strftime("%Y-%m-%dT%H:%M:%S.%f+00:00")  # getting minimum date
+
+        settings.pdef_method = PDFMethod.DEFAULT
+        input = ParameterExtractionInputForStochasticMiner(
+            log_traces=self.log_train.get_traces(), log_traces_raw=self.log_train.get_raw_traces(), bpmn=bpmn,
+            bpmn_graph=self.bpmn_graph, process_graph=process_graph, settings=settings)
+        output = ParameterExtractionOutput()
+        output.process_stats['role'] = 'SYSTEM'  # TODO: what is this for?
+        structure_mining_pipeline = Pipeline(input=input, output=output)
+        structure_mining_pipeline.set_pipeline([
+            LogReplayerForStructureOptimizer,
+            ResourceMinerForStructureOptimizer,
+            InterArrivalMiner,
+            GatewayProbabilitiesMinerForStochasticMiner,
+            TasksProcessor
+        ])
+        structure_mining_pipeline.execute()
+
+        parameters = {**parameters, **{'resource_pool': output.resource_pool,
+                                       'time_table': output.time_table,
+                                       'arrival_rate': output.arrival_rate,
+                                       'sequences': output.sequences,
+                                       'elements_data': output.elements_data,
+                                       'instances': num_inst,
+                                       'start_time': start_time}}
+        bpmn_path = os.path.join(settings.output, settings.project_name + '.bpmn')
         xml_writer.print_parameters(bpmn_path, bpmn_path, parameters)
 
         self.log_valdn.rename(columns={'user': 'resource'}, inplace=True)
@@ -329,22 +348,17 @@ class StructureOptimizerForStochasticProcessMiner(StructureOptimizer):
 # Parameters Extraction Implementations: For Stochastic Process Miner
 
 @dataclass
-class ParameterExtractionInputForStochasticMiner:
-    log_traces: list = None
-    bpmn: BPMNGraph = None
-
-
-@dataclass
-class ParameterExtractionOutputForStochasticMiner:
-    sequences: list = field(default_factory=list)
+class ParameterExtractionInputForStochasticMiner(ParameterExtractionInput):
+    log_traces_raw: list = None
+    bpmn_graph: BPMNGraph = None
 
 
 class GatewayProbabilitiesMinerForStochasticMiner(Operator):
     input: ParameterExtractionInputForStochasticMiner
-    output: ParameterExtractionOutputForStochasticMiner
+    output: ParameterExtractionOutput
 
     def __init__(self, input: ParameterExtractionInputForStochasticMiner,
-                 output: ParameterExtractionOutputForStochasticMiner):
+                 output: ParameterExtractionOutput):
         self.input = input
         self.output = output
         self._execute()
@@ -353,7 +367,7 @@ class GatewayProbabilitiesMinerForStochasticMiner(Operator):
         print_step('Mining gateway probabilities')
 
         arcs_frequencies = self._compute_sequence_flow_frequencies()
-        gateways_branching = self.input.bpmn.compute_branching_probability(arcs_frequencies)
+        gateways_branching = self.input.bpmn_graph.compute_branching_probability(arcs_frequencies)
 
         sequences = []
         for gateway_id in gateways_branching:
@@ -365,12 +379,12 @@ class GatewayProbabilitiesMinerForStochasticMiner(Operator):
 
     def _compute_sequence_flow_frequencies(self):
         flow_arcs_frequency = dict()
-        for trace in self.input.log_traces:
+        for trace in self.input.log_traces_raw:
             task_sequence = list()
             for event in trace:
                 task_name = event['task']            # original: concept:name
                 state = event['event_type'].lower()  # original: lifecycle:transition
                 if state in ["start", "assign"]:
                     task_sequence.append(task_name)
-            self.input.bpmn.replay_trace(task_sequence, flow_arcs_frequency)
+            self.input.bpmn_graph.replay_trace(task_sequence, flow_arcs_frequency)
         return flow_arcs_frequency
