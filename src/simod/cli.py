da03887c19033faeb469a3e90bc861e4ebe4cd7d
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-08-02 12:43:53 +0300

    Cleaning up cli.py

--- src/simod/cli.py
+++ src/simod/cli.py
@@ -1,11 +1,11 @@
-import copy
+import os
+from pathlib import Path
+
 import os
 from pathlib import Path
 
 import click
-import utils.support as sup
-from simod.configuration import Configuration, AlgorithmManagement, DataType, \
-    GateManagement, Metric, ExecutionMode, config_data_from_file
+from simod.configuration import Configuration, config_data_from_file
 from simod.discoverer import Discoverer
 from simod.optimizer import Optimizer
 from simod.stochastic_miner import StructureOptimizerForStochasticProcessMiner
@@ -57,52 +57,6 @@ def optimize(ctx, config_path):
     tm_data.update(global_data)
     time_optimizer_config = Configuration(**tm_data)
     time_optimizer_config.fill_in_derived_fields()
-
-
-    # global_config = Configuration(**config_data)
-    # global_config.fill_in_derived_fields()
-    #
-    # global_config = Configuration(input=Path(os.path.join(repository_dir, 'inputs')),
-    #                               output=Path(os.path.join(repository_dir, 'outputs', sup.folder_id())),
-    #                               exec_mode=ExecutionMode.OPTIMIZER,
-    #                               repetitions=1,
-    #                               # repetitions=5,
-    #                               simulation=True,
-    #                               sim_metric=Metric.DL,
-    #                               # sim_metric=Metric.TSD,
-    #                               add_metrics=[],
-    #                               # add_metrics=[Metric.DAY_HOUR_EMD,
-    #                               #              Metric.LOG_MAE, Metric.DL,
-    #                               #              Metric.MAE],
-    #                               **ctx.params)
-    # global_config.fill_in_derived_fields()
-
-    # structure_optimizer_config = copy.copy(global_config)
-    # structure_optimizer_config.max_eval_s = 2
-    # # structure_optimizer_config.max_eval_s = 15
-    # structure_optimizer_config.concurrency = [0.0, 1.0]
-    # structure_optimizer_config.epsilon = [0.0, 1.0]
-    # structure_optimizer_config.eta = [0.0, 1.0]
-    # structure_optimizer_config.alg_manag = [AlgorithmManagement.REMOVAL]
-    # # structure_optimizer_config.alg_manag = [AlgorithmManagement.REPAIR,
-    # #                                         AlgorithmManagement.REMOVAL,
-    # #                                         AlgorithmManagement.REPLACEMENT]
-    # structure_optimizer_config.gate_management = [GateManagement.EQUIPROBABLE]
-    # # structure_optimizer_config.gate_management = [GateManagement.DISCOVERY,
-    # #                                               GateManagement.EQUIPROBABLE]
-
-    # time_optimizer_config = copy.copy(global_config)
-    # time_optimizer_config.max_eval_t = 2
-    # # time_optimizer_config.max_eval_t = 20
-    # time_optimizer_config.rp_similarity = [0.5, 0.9]
-    # time_optimizer_config.res_dtype = [DataType.DT247]
-    # # [DataType.DT247, DataType.LV917]
-    # time_optimizer_config.arr_dtype = [DataType.DT247]
-    # # [DataType.DT247, DataType.LV917]
-    # time_optimizer_config.res_sup_dis = [0.01, 0.3]
-    # time_optimizer_config.res_con_dis = [50, 85]
-    # time_optimizer_config.arr_support = [0.01, 0.1]
-    # time_optimizer_config.arr_confidence = [1, 10]
 
     optimizer = Optimizer({'gl': global_config, 'strc': structure_optimizer_config, 'tm': time_optimizer_config})
     if global_config.new_replayer:
