26d810016c21b6e9f8c720373838f87726d0d743
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-08-10 15:07:21 +0300

    Tests updated

--- src/simod/common_routines_test.py
+++ src/simod/common_routines_test.py
@@ -9,7 +9,7 @@ from typing import List, Tuple
 
 import pandas as pd
 from simod.common_routines import compute_sequence_flow_frequencies, extract_structure_parameters, \
-    extract_process_graph, mine_gateway_probabilities_stochastic, mine_gateway_probabilities_stochastic_alternative
+    extract_process_graph, mine_gateway_probabilities_stochastic_alternative
 from simod.configuration import Configuration, GateManagement
 from simod.readers.log_reader import LogReader
 from simod.readers.log_splitter import LogSplitter
@@ -18,7 +18,7 @@ from simod.structure_optimizer import StructureOptimizer
 
 
 class TestReplayer(unittest.TestCase):
-    args: List[dict] = [
+    validation_1_args: List[dict] = [
         {'model_path': Path(os.path.dirname(__file__) + '/../../test_assets/PurchasingExample.bpmn'),
          'log_path': Path(os.path.dirname(__file__) + '/../../test_assets/PurchasingExample.xes')},
         {'model_path': Path(os.path.dirname(
@@ -35,6 +35,10 @@ class TestReplayer(unittest.TestCase):
              __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_E7C625FF_E3CA_4AB3_A386_901182018864/BPI_Challenge_2012_W_Two_TS.xes')},
     ]
 
+    validation_2_args: List[dict] = [
+        {'log_path': Path(os.path.dirname(__file__) + '/../../test_assets/validation_2/BPI_Challenge_2017_W_Two_TS.xes')}
+    ]
+
     @staticmethod
     def setup_data(model_path: Path, log_path: Path):
         settings = Configuration(model_path=Path(model_path), log_path=Path(log_path))
@@ -45,8 +49,44 @@ class TestReplayer(unittest.TestCase):
 
         return graph, log, settings
 
+    @staticmethod
+    def split_log_buckets(log: LogReader, size: float, one_ts: bool) -> Tuple[pd.DataFrame, LogReader]:
+        # Split log data
+        splitter = LogSplitter(log.data)
+        train, test = splitter.split_log('timeline_contained', size, one_ts)
+        total_events = len(log.data)
+
+        # Check size and change time splitting method if necesary
+        if len(test) < int(total_events * 0.1):
+            train, test = splitter.split_log('timeline_trace', size, one_ts)
+
+        # Set splits
+        key = 'end_timestamp' if one_ts else 'start_timestamp'
+        test = pd.DataFrame(test)
+        train = pd.DataFrame(train)
+        log_test = test.sort_values(key, ascending=True).reset_index(drop=True)
+        log_train = copy.deepcopy(log)
+        log_train.set_data(train.sort_values(key, ascending=True).reset_index(drop=True).to_dict('records'))
+
+        return log_test, log_train
+
+    @staticmethod
+    def discover_model(settings: Configuration) -> Tuple[Path, LogReader, pd.DataFrame]:
+        log = LogReader(settings.log_path, settings.read_options)
+
+        if not os.path.exists(settings.output.parent):
+            os.makedirs(settings.output.parent)
+
+        log_test, log_train = TestReplayer.split_log_buckets(log, 0.8, settings.read_options.one_timestamp)
+
+        structure_optimizer = StructureOptimizer(settings, copy.deepcopy(log_train), discover_model=True)
+        structure_optimizer.execute_trials()
+        model_path = Path(os.path.join(structure_optimizer.best_output, settings.project_name + '.bpmn'))
+
+        return model_path, log_train, log_test
+
     def test_replay_trace(self):
-        for arg in self.args:
+        for arg in self.validation_1_args:
             model_path = arg['model_path']
             log_path = arg['log_path']
             print(f'Testing {log_path.name}')
@@ -75,7 +115,7 @@ class TestReplayer(unittest.TestCase):
                 self.fail(f'Should not fail, failed with: {e}')
 
     def test_compute_sequence_flow_frequencies(self):
-        for arg in self.args:
+        for arg in self.validation_1_args:
             model_path = arg['model_path']
             log_path = arg['log_path']
             print(f'Testing {log_path.name}')
@@ -94,54 +134,21 @@ class TestReplayer(unittest.TestCase):
                 self.assertFalse(flow_arcs_frequency[node_id] == 0)
 
     def test_compute_sequence_flow_frequencies_without_model(self):
-        def split_log_buckets(log: LogReader, size: float, one_ts: bool) -> Tuple[pd.DataFrame, LogReader]:
-            # Split log data
-            splitter = LogSplitter(log.data)
-            train, test = splitter.split_log('timeline_contained', size, one_ts)
-            total_events = len(log.data)
-
-            # Check size and change time splitting method if necesary
-            if len(test) < int(total_events * 0.1):
-                train, test = splitter.split_log('timeline_trace', size, one_ts)
-
-            # Set splits
-            key = 'end_timestamp' if one_ts else 'start_timestamp'
-            test = pd.DataFrame(test)
-            train = pd.DataFrame(train)
-            log_test = test.sort_values(key, ascending=True).reset_index(drop=True)
-            log_train = copy.deepcopy(log)
-            log_train.set_data(train.sort_values(key, ascending=True).reset_index(drop=True).to_dict('records'))
-
-            return log_test, log_train
-
-        def discover_model(settings: Configuration) -> Tuple[Path, LogReader, pd.DataFrame]:
-            log = LogReader(settings.log_path, settings.read_options)
-
-            if not os.path.exists(settings.output.parent):
-                os.makedirs(settings.output.parent)
-
-            log_test, log_train = split_log_buckets(log, 0.8, settings.read_options.one_timestamp)
-
-            # settings for StructureOptimizer
-            settings.max_eval_s = 2
-            settings.concurrency = [0.0, 1.0]
-            settings.epsilon = [0.0, 1.0]
-            settings.eta = [0.0, 1.0]
-            settings.gate_management = [GateManagement.DISCOVERY]
-
-            structure_optimizer = StructureOptimizer(settings, copy.deepcopy(log_train), discover_model=True)
-            structure_optimizer.execute_trials()
-            model_path = Path(os.path.join(structure_optimizer.best_output, settings.project_name + '.bpmn'))
-
-            return model_path, log_train, log_test
-
-        for arg in self.args:
+        for arg in self.validation_1_args:
             log_path = arg['log_path']
             print(f'\n\nTesting {log_path.name}')
 
-            config = Configuration(log_path=log_path)  # TODO: fix output path error
+            config = Configuration(log_path=log_path)
             config.fill_in_derived_fields()
-            model_path, log_train, _ = discover_model(config)
+
+            # settings for StructureOptimizer
+            config.max_eval_s = 2
+            config.concurrency = [0.0, 1.0]
+            config.epsilon = [0.0, 1.0]
+            config.eta = [0.0, 1.0]
+            config.gate_management = [GateManagement.DISCOVERY]
+
+            model_path, log_train, _ = TestReplayer.discover_model(config)
             print(f'\nmodel_path = {model_path}\n')
 
             graph = BPMNGraph.from_bpmn_path(model_path)
@@ -176,12 +183,12 @@ class TestReplayer(unittest.TestCase):
     #         self.assertFalse(len(sequences) == 0)
 
     def test_mine_gateway_probabilities_stochastic_alternative(self):
-        for arg in self.args:
+        for arg in self.validation_1_args:
             model_path = arg['model_path']
             log_path = arg['log_path']
             print(f'\nTesting {log_path.name}')
 
-            graph, log, _ = self.setup_data(model_path, log_path)
+            graph, log, _ = TestReplayer.setup_data(model_path, log_path)
             traces = log.get_raw_traces()
 
             try:
@@ -194,12 +201,12 @@ class TestReplayer(unittest.TestCase):
             self.assertFalse(len(sequences) == 0)
 
     def test_extract_structure_parameters(self):
-        for arg in self.args:
+        for arg in self.validation_2_args:
             model_path = arg['model_path']
             log_path = arg['log_path']
             print(f'\nTesting {log_path.name}')
 
-            graph, log, settings = self.setup_data(model_path, log_path)
+            graph, log, settings = TestReplayer.setup_data(model_path, log_path)
             process_graph = extract_process_graph(model_path)
 
             try:
@@ -215,6 +222,37 @@ class TestReplayer(unittest.TestCase):
             self.assertTrue(parameters.time_table is not None)
             self.assertTrue(parameters.sequences is not None)
             self.assertTrue(parameters.elements_data is not None)
+
+    def test_compute_sequence_flow_frequencies_without_model_validation_2(self):
+        for arg in self.validation_2_args:
+            log_path = arg['log_path']
+            print(f'\n\nTesting {log_path.name}')
+
+            config = Configuration(log_path=log_path)
+            config.fill_in_derived_fields()
+
+            # settings for StructureOptimizer
+            config.max_eval_s = 2
+            config.concurrency = [0.0, 1.0]
+            config.epsilon = [0.0, 1.0]
+            config.eta = [0.0, 1.0]
+            config.gate_management = [GateManagement.DISCOVERY]
+
+            model_path, log_train, _ = TestReplayer.discover_model(config)
+            print(f'\nmodel_path = {model_path}\n')
+
+            graph = BPMNGraph.from_bpmn_path(model_path)
+            traces = log_train.get_raw_traces()
+
+            try:
+                flow_arcs_frequency = compute_sequence_flow_frequencies(traces, graph)
+            except Exception as e:
+                self.fail(f'Should not fail, failed with: {e}')
+
+            self.assertTrue(flow_arcs_frequency is not None)
+            self.assertTrue(len(flow_arcs_frequency) > 0)
+            for node_id in flow_arcs_frequency:
+                self.assertFalse(flow_arcs_frequency[node_id] == 0)
 
 
 if __name__ == '__main__':
