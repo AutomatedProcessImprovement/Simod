2307f25476a643a963e1fe7e0b15fec222ed851a
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-08-13 12:41:05 +0300

    Changing paths in validation_1 tests to full logs

--- src/simod/common_routines_test.py
+++ src/simod/common_routines_test.py
@@ -24,15 +24,15 @@ class TestReplayer(unittest.TestCase):
         {'model_path': Path(os.path.dirname(
             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_48BA9CAF_B626_44EC_808E_FBEBCC6CF52C/Production.bpmn'),
          'log_path': Path(os.path.dirname(
-             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_48BA9CAF_B626_44EC_808E_FBEBCC6CF52C/Production.xes')},
+             __file__) + '/../../test_assets/validation_1/complete logs/Production.xes')},
         {'model_path': Path(os.path.dirname(
             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_672EE52F_F905_4860_9CD2_57F95917D1C9/ConsultaDataMining201618.bpmn'),
          'log_path': Path(os.path.dirname(
-             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_672EE52F_F905_4860_9CD2_57F95917D1C9/ConsultaDataMining201618.xes')},
+             __file__) + '/../../test_assets/validation_1/complete logs/ConsultaDataMining201618.xes')},
         {'model_path': Path(os.path.dirname(
             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_E7C625FF_E3CA_4AB3_A386_901182018864/BPI_Challenge_2012_W_Two_TS.bpmn'),
          'log_path': Path(os.path.dirname(
-             __file__) + '/../../test_assets/validation_1/testing logs and models/20210804_E7C625FF_E3CA_4AB3_A386_901182018864/BPI_Challenge_2012_W_Two_TS.xes')},
+             __file__) + '/../../test_assets/validation_1/complete logs/BPI_Challenge_2012_W_Two_TS.xes')},
     ]
 
     validation_2_args: List[dict] = [
