e8b0a7f7d9c27d44ad42384c954b87ce10156098
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-12-17 22:08:43 +0200

    Jenkins setup

---
+++ Jenkinsfile
@@ -0,0 +1,12 @@
+pipeline {
+    agent {
+        docker { image 'nokal/simod-testing:v1.1.0' }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'cd /usr/src && bash test_simod.sh'
+            }
+        }
+    }
+}
---
+++ jenkins.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# https://www.jenkins.io/doc/book/installing/docker/#setup-wizard
+
+docker run \
+  --name jenkins-docker \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+
+docker run --name jenkins-simod --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  nokal/simod-jenkins
