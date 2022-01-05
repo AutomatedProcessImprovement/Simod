01c5312c54ffe27a76e5563c926e4e2782030b0b
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-01-05 16:44:17 +0200

    Changing simod version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simod',
-    version='0.2.0',
+    version='3.0.0',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     include_package_data=True,
