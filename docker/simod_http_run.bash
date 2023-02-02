fb6e1acf5126e94d35d08711cc0238028d61dbec
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-02-02 13:10:34 +0200

    Simod HTTP bash script updated

--- docker/simod_http_run.bash
+++ docker/simod_http_run.bash
@@ -2,8 +2,5 @@
 
 Xvfb :99 &>/dev/null & disown
 
-cd /usr/src/Simod
-source $(poetry env info --path)/bin/activate
-
 cd /usr/src/Simod/src/simod_http
-uvicorn simod_http.main:app --host 0.0.0.0 --port 80
+poetry run uvicorn simod_http.main:app --host 0.0.0.0 --port 80
