430d7b564234550866e17599cecc11539f368d3d
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-02-13 13:26:48 +0200

    Simod HTTP uses FastAPI default form data management API instead of custom multipart/mixed parsing

--- src/simod_http/main.py
+++ src/simod_http/main.py
@@ -2,21 +2,19 @@ import logging
 import os
 import shutil
 from pathlib import Path
-from typing import Callable, Union, Optional
+from typing import Union, Optional
 
 import pandas as pd
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks, Request, Response, Body, HTTPException
+from fastapi import FastAPI, BackgroundTasks, Request, Response, Form
 from fastapi.responses import JSONResponse
-from fastapi.routing import APIRoute
 from fastapi_utils.tasks import repeat_every
-from pydantic import ValidationError
 from uvicorn.config import LOGGING_CONFIG
 
 from simod.configuration import Configuration
 from simod.event_log.utilities import read as read_event_log
 from simod_http.app import Response as AppResponse, RequestStatus, Request as AppRequest, Settings, NotFound, \
-    BadMultipartRequest, UnsupportedMediaType, BaseRequestException, Error, NotificationSettings, NotificationMethod, \
+    UnsupportedMediaType, BaseRequestException, NotificationSettings, NotificationMethod, \
     NotSupported
 from simod_http.archiver import make_url_for
 from simod_http.executor import Executor
@@ -33,6 +31,8 @@ settings.simod_http_storage_path = Path(settings.simod_http_storage_path)
 app = FastAPI()
 
 
+# Background tasks
+
 def run_simod_discovery(request: Request, settings: Settings):
     """
     Run Simod with the user's configuration.
@@ -41,50 +41,7 @@ def run_simod_discovery(request: Request, settings: Settings):
     executor.run()
 
 
-class CustomRequest(Request):
-    async def body(self) -> list[bytes]:
-        if not hasattr(self, '_body'):
-            body = await super().body()
-
-            # If the request multipart, then the body is a list of bytes split by the boundary specified in the header
-            content_type = self.headers.get('content-type', '')
-            if 'multipart/mixed' in content_type or 'multipart/form-data' in content_type:
-                boundary = self.headers['content-type'].split('boundary=')[1]
-                boundary = boundary.strip('"')
-                body = body.split(f'--{boundary}\n'.encode())[1:]
-
-            self._body = body
-
-        return self._body
-
-
-class CustomRouteMatcher(APIRoute):
-    def get_route_handler(self) -> Callable:
-        original_route_handler = super().get_route_handler()
-
-        async def custom_route_handler(request: Request) -> Response:
-            try:
-                request = CustomRequest(request.scope, request.receive)
-                return await original_route_handler(request)
-            except Exception as e:
-                if isinstance(e, BaseRequestException):
-                    return e.json_response()
-                elif isinstance(e, ValidationError):
-                    err = Error(message=str(e), details=e.errors())
-                elif isinstance(e, HTTPException):
-                    err = Error(message=e.detail)
-                else:
-                    err = Error(message=str(e), details=repr(e))
-
-                response = AppResponse.construct()
-                response.error = err
-                return response.json_response(status_code=500)
-
-        return custom_route_handler
-
-
-app.router.route_class = CustomRouteMatcher
-
+# Hooks
 
 @app.on_event('startup')
 async def application_startup():
@@ -185,6 +142,8 @@ async def request_exception_handler(_, exc: BaseRequestException) -> JSONRespons
     return exc.json_response()
 
 
+# Routes
+
 @app.get('/discoveries/{request_id}/{file_name}')
 async def read_discovery_file(request_id: str, file_name: str):
     """
@@ -228,7 +187,8 @@ async def read_discovery(request_id: str) -> AppResponse:
 @app.post('/discoveries')
 async def create_discovery(
         background_tasks: BackgroundTasks,
-        bodies: list[bytes] = Body(),
+        configuration=Form(),
+        event_log=Form(),
         callback_url: Optional[str] = None,
         email: Optional[str] = None,
 ) -> JSONResponse:
@@ -240,13 +200,45 @@ async def create_discovery(
     request = await _empty_request_from_params(settings.simod_http_storage_path, callback_url, email)
 
     if email is not None:
+        request.status = RequestStatus.FAILURE
+        request.save()
+
         raise NotSupported(
             request_id=request.id,
-            request_status=RequestStatus.FAILURE,
+            request_status=request.status,
             message='Email notifications are not supported',
         )
 
-    request = await _parse_post_body(bodies, request)
+    # Configuration
+
+    configuration = Configuration.from_stream(configuration.file)
+
+    # Event log
+
+    event_log_file_extension = _infer_event_log_file_extension_from_header(event_log.content_type)
+    if event_log_file_extension is None:
+        raise UnsupportedMediaType(
+            request_id=request.id,
+            request_status=request.status,
+            archive_url=None,
+            message='Unsupported event log file type',
+        )
+
+    event_log_path = request.output_dir / f'event_log{event_log_file_extension}'
+    event_log_path.write_bytes(event_log.file.read())
+
+    configuration.common.log_path = event_log_path.absolute()
+    configuration.common.test_log_path = None
+
+    event_log, event_log_csv_path = read_event_log(configuration.common.log_path, configuration.common.log_ids)
+
+    request.configuration = configuration
+    request.event_log = event_log
+    request.event_log_csv_path = event_log_csv_path
+    request.status = RequestStatus.ACCEPTED
+    request.save()
+
+    # Response
 
     response = AppResponse(request_id=request.id, request_status=request.status)
 
@@ -264,30 +256,7 @@ async def root() -> JSONResponse:
     )
 
 
-async def _parse_post_body(bodies: list[bytes], request: AppRequest) -> AppRequest:
-    configuration, event_log, event_log_csv_path = _parse_bodies_from_request(
-        request.id,
-        request.status,
-        bodies,
-        request.output_dir,
-    )
-
-    if event_log is None:
-        raise NotFound(
-            request_id=request.id,
-            request_status=request.status,
-            archive_url=request.archive_url,
-            message='Event log not found',
-        )
-
-    request.configuration = configuration
-    request.event_log = event_log
-    request.event_log_csv_path = event_log_csv_path
-    request.status = RequestStatus.ACCEPTED
-    request.save()
-
-    return request
-
+# Helpers
 
 async def _empty_request_from_params(
         storage_path: str,
@@ -314,141 +283,15 @@ async def _empty_request_from_params(
     return request
 
 
-def _parse_bodies_from_request(
-        request_id: str,
-        request_status: RequestStatus,
-        files: list[bytes],
-        output_dir: Path,
-) -> tuple[Configuration, Union[pd.DataFrame, None], Union[Path, None]]:
-    configuration = None
-    event_log = None
-    event_log_file_extension = None
-    bpmn_model = None
-
-    for content in files:
-        parts = content.split(b'\n\n')
-        if len(parts) >= 2:
-            header = _parse_headers(parts[0])
-            body = parts[1]
-        else:
-            raise BadMultipartRequest(
-                request_id=request_id,
-                request_status=request_status,
-                message='Each part of the multipart request must have a header and a body',
-            )
-
-        filename = _get_multipart_filename_from_header(header)
-
-        if _is_header_yaml(header.get('content-type')) \
-                or '.yaml' in filename \
-                or '.yml' in filename:
-            configuration = Configuration.from_stream(body)
-            continue
-
-        if '.bpmn' in filename:
-            bpmn_model = body
-            continue
-
-        event_log = body
-        event_log_file_extension = _infer_event_log_file_extension_from_header(
-            request_id,
-            request_status,
-            header.get('content-type')
-        )
-
-    if configuration is None:
-        configuration = Configuration.default()
-
-    if bpmn_model is not None:
-        bpmn_path = output_dir / f'model.bpmn'
-        bpmn_path.write_bytes(bpmn_model)
-
-        configuration.common.model_path = bpmn_path.absolute()
-
-    csv_path = None
-
-    if event_log is not None:
-        event_log_path = output_dir / f'event_log{event_log_file_extension}'
-        event_log_path.write_bytes(event_log)
-
-        configuration.common.log_path = event_log_path.absolute()
-        configuration.common.test_log_path = None
-
-        event_log, csv_path = read_event_log(configuration.common.log_path, configuration.common.log_ids)
-
-    return configuration, event_log, csv_path
-
-
-def _parse_headers(header: bytes) -> dict[str, str]:
-    headers = {}
-    for line in header.split(b'\n'):
-        if line:
-            key, value = line.split(b':')
-            key = key.decode('utf-8').strip().lower()
-            value = value.decode('utf-8').strip().rstrip(';').lower()
-            if ';' in value:
-                value_parts = value.split(';')
-                value = []
-                for value_part in value_parts:
-                    if '=' in value_part:
-                        value_part_key, value_part_value = value_part.split('=')
-                        value_part_key = value_part_key.strip()
-                        value_part_value = value_part_value.strip().strip('"')
-                        value.append({value_part_key: value_part_value})
-                    else:
-                        value.append({'value': value_part})
-            headers[key] = value
-    return headers
-
-
-def _get_multipart_filename_from_header(header: dict[str, str]) -> str:
-    content = header.get('content-disposition', [])
-
-    if len(content) == 0:
-        return ''
-
-    names = list(
-        map(lambda x: x.get('filename', ''),
-            filter(lambda x: 'filename' in x, content))
-    )
-
-    if len(names) == 0:
-        return ''
-
-    return names[0]
-
-
-def _is_header_yaml(header: str) -> bool:
-    return 'application/x-yaml' in header or \
-        'application/yaml' in header or \
-        'text/yaml' in header or \
-        'text/x-yaml' in header or \
-        'text/vnd.yaml' in header
-
-
-def _is_header_xml(header: str) -> bool:
-    return 'application/xml' in header or \
-        'text/xml' in header or \
-        'text/x-xml' in header or \
-        'text/vnd.xml' in header
-
-
 def _infer_event_log_file_extension_from_header(
-        request_id: str,
-        request_status: RequestStatus,
         content_type: str,
-) -> str:
+) -> Union[str, None]:
     if 'text/csv' in content_type:
         return '.csv'
     elif 'application/xml' in content_type or 'text/xml' in content_type:
         return '.xml'
     else:
-        raise UnsupportedMediaType(
-            request_id=request_id,
-            request_status=request_status,
-            archive_url=None,
-            message='Unsupported event log file type',
-        )
+        return None
 
 
 async def _infer_media_type_from_extension(file_name) -> str:
