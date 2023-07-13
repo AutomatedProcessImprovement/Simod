5d4c08160fa62b31fcd06b107da21a13173c4b11
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-07-13 13:01:38 +0300

    Refactor docker_jobs from benchmarking

--- benchmarking/docker_jobs.py
+++ benchmarking/docker_jobs.py
@@ -1,39 +1,141 @@
 import subprocess
 import time
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional
 
-config_str = Path("input/config.yml").read_text()
+# Simod configuration string to generate the configuration file for each experiment
+configuration_str = Path("input/config.yml").read_text()
+
+# Path to the directory containing the logs and configuration files in the Docker container
+container_input_dir = Path("/usr/src/Simod/input")
 
 
-def create_configuration_file(
-    log_path: Path,
-    config_base: str,
-    host_config_dir: Path,
-    container_input_dir: Path,
-    test_log_path: Optional[Path] = None,
-    prefix: Optional[str] = None,
-):
+@dataclass
+class Experiment:
     """
-    Creates a configuration file
+    Dataclass representing an experiment with a train and, optionally, test log, and the corresponding configuration.
     """
-    config = config_base.replace("<train_log_path>", str((container_input_dir / "logs" / log_path.name).absolute()))
-    if test_log_path:
-        config = config.replace("<test_log_path>", str((container_input_dir / "logs" / test_log_path.name).absolute()))
 
-    if prefix:
-        config_path = (host_config_dir / (Path(log_path).stem + prefix)).with_suffix(".yml")
-    else:
-        config_path = (host_config_dir / Path(log_path).stem).with_suffix(".yml")
+    train_log_path: Path
+    test_log_path: Optional[Path] = None
 
-    with open(config_path, "w") as f:
-        f.write(config)
+    _assets_dir: Path = Path("input")
+    _configuration_dir: Path = Path("input/configs")
+    _logs_dir: Path = Path("input/logs")
+    _experiments_output_dir: Path = Path("outputs")
 
-    return config_path
+    @property
+    def assets_dir(self) -> Path:
+        """
+        Directory containing the assets (logs, configurations, etc.) for the experiment on the host machine.
+        """
+        return self._assets_dir
+
+    @property
+    def experiments_output_dir(self) -> Path:
+        """
+        Directory containing the outputs of the experiments on the host machine.
+        """
+        return self._experiments_output_dir
+
+    @property
+    def configuration_path(self) -> Path:
+        """
+        Path to the configuration file for the experiment on the host machine.
+        """
+        return self._configuration_dir / f"{self.train_log_path.stem}.yml"
+
+    def __post_init__(self):
+        assert self._assets_dir.exists()
+        assert self._logs_dir.exists()
+        self._configuration_dir.mkdir(exist_ok=True, parents=True)
+        self._generate_configuration()
+
+    def _generate_configuration(self):
+        """
+        Update the configuration with the paths to the logs and save it to the configuration directory
+        on the host machine.
+        """
+        global container_input_dir, configuration_str
+
+        config = configuration_str.replace(
+            "<train_log_path>", str((container_input_dir / "logs" / self.train_log_path.name).absolute())
+        )
+        if self.test_log_path:
+            config = config.replace(
+                "<test_log_path>", str((container_input_dir / "logs" / self.test_log_path.name).absolute())
+            )
+
+        self.configuration_path.write_text(config)
 
 
-def create_job_script(jobs_dir: Path, config_path: Path, prefix: Optional[str] = None):
-    """Create a job script"""
+def main():
+    experiments = [
+        Experiment(
+            train_log_path=Path("logs/AcademicCredentials_train.csv.gz"),
+            test_log_path=Path("logs/AcademicCredentials_test.csv.gz"),
+        ),
+        Experiment(
+            train_log_path=Path("logs/CallCenter_train.csv.gz"),
+            test_log_path=Path("logs/CallCenter_test.csv.gz"),
+        ),
+        # Experiment(
+        #     train_log_path=Path("logs/BPIC_2012_train.csv.gz"),
+        #     test_log_path=Path("logs/BPIC_2012_test.csv.gz"),
+        # ),
+        # Experiment(
+        #     train_log_path=Path("logs/BPIC_2017_train.csv.gz"),
+        #     test_log_path=Path("logs/BPIC_2017_test.csv.gz"),
+        # ),
+    ]
+
+    with Path("timing.txt").open("a") as f:
+        for experiment in experiments:
+            print(f"Running {experiment.configuration_path}")
+            start = time.time()
+            run_with_docker(experiment)
+            duration = time.time() - start
+            f.write(f"{experiment.configuration_path.stem}: {duration}\n")
+            # flushing saves each record to disk (not guaranteed though) without waiting for other experiments to finish
+            f.flush()
+            print(f"Finished {experiment.configuration_path} in {duration} seconds")
+
+
+def run_with_docker(experiment: Experiment):
+    global container_input_dir
+
+    docker_run_script = f"""#!/bin/bash
+
+cd /usr/src/Simod
+Xvfb :99 &>/dev/null & disown
+poetry run simod optimize --config_path {container_input_dir}/configs/{experiment.configuration_path.name}
+"""
+    docker_run_script_path = experiment.assets_dir / "docker_run.sh"
+    docker_run_script_path.write_text(docker_run_script)
+
+    docker_run_script_path_in_container = container_input_dir / "docker_run.sh"
+    cmd = [
+        "docker",
+        "run",
+        "-v",
+        f"{experiment.assets_dir.absolute()}:/usr/src/Simod/input",
+        "-v",
+        f"{experiment.experiments_output_dir.absolute()}:/usr/src/Simod/outputs",
+        "nokal/simod",
+        "bash",
+        docker_run_script_path_in_container.absolute(),
+    ]
+
+    with open(f"{experiment.configuration_path.stem}.out", "w") as f:
+        with open(f"{experiment.configuration_path.stem}.err", "w") as g:
+            subprocess.run(cmd, stdout=f, stderr=g)
+
+
+def create_slurm_job_script(jobs_dir: Path, config_path: Path, prefix: Optional[str] = None):
+    """
+    Create a SLURM job script for the given configuration file to run in the HPC.
+    """
 
     job_name = config_path.stem
     partition = "main"
@@ -64,80 +166,6 @@ xvfb-run simod optimize --config_path {config_path.absolute()}
         f.write(script)
 
     return job_script
-
-
-def run_docker_job(config_path: Path, host_input_dir: Path, host_output_dir: Path):
-    """Run a job in a Docker container"""
-    container_base_path = Path("/usr/src/Simod/input")
-
-    docker_run_script = f"""#!/bin/bash
-
-cd /usr/src/Simod
-Xvfb :99 &>/dev/null & disown
-poetry run simod optimize --config_path {container_base_path}/configs/{config_path.name}
-"""
-
-    docker_run_script_path = host_input_dir / "docker_run.sh"
-    docker_run_container_script_path = container_base_path / "docker_run.sh"
-
-    with open(docker_run_script_path, "w") as f:
-        f.write(docker_run_script)
-
-    cmd = [
-        "docker",
-        "run",
-        "-v",
-        f"{host_input_dir.absolute()}:/usr/src/Simod/input",
-        "-v",
-        f"{host_output_dir.absolute()}:/usr/src/Simod/outputs",
-        "nokal/simod",
-        "bash",
-        docker_run_container_script_path.absolute(),
-    ]
-
-    with open(f"{config_path.stem}.out", "w") as f:
-        with open(f"{config_path.stem}.err", "w") as g:
-            subprocess.run(cmd, stdout=f, stderr=g)
-
-
-def main():
-    container_input_dir = Path("/usr/src/Simod/input")
-
-    host_input_dir = Path("input")
-    config_dir = host_input_dir / "configs"
-    config_dir.mkdir(exist_ok=True, parents=True)
-
-    host_output_dir = Path("outputs")
-    host_output_dir.mkdir(exist_ok=True, parents=True)
-
-    log_paths = [
-        # (Path("logs/AcademicCredentials_train.csv.gz"), Path("logs/AcademicCredentials_test.csv.gz")),
-        (Path("logs/BPIC_2012_train.csv.gz"), Path("logs/BPIC_2012_test.csv.gz")),
-        (Path("logs/BPIC_2017_train.csv.gz"), Path("logs/BPIC_2017_test.csv.gz")),
-        # (Path("logs/CallCenter_train.csv.gz"), Path("logs/CallCenter_test.csv.gz")),
-    ]
-
-    config_paths = [
-        create_configuration_file(
-            host_input_dir / log_path[0],
-            config_str,
-            config_dir,
-            container_input_dir,
-            test_log_path=host_input_dir / log_path[1],
-        )
-        for log_path in log_paths
-    ]
-
-    timing_file = Path("timing.txt")
-
-    with open(timing_file, "a") as f:
-        for config_path in config_paths:
-            print(f"Running {config_path}")
-            start = time.time()
-            run_docker_job(config_path, host_input_dir, host_output_dir)
-            duration = time.time() - start
-            f.write(f"{config_path.stem}: {duration}\n")
-            print(f"Finished {config_path} in {duration} seconds")
 
 
 if __name__ == "__main__":
